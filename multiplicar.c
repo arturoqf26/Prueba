55437cc1156836025f9755b95473d7e9efd4d3cd
Jesús Arturo Quirós Fallas <83099103+arturoqf26@users.noreply.github.com>
2021-06-25 17:45:49 -0600

    aaa

---
+++ multiplicar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int multiplicacion(int a, int b) {
+    if(b==0){
+        return 0;
+    }
+    else if (b==1){
+        return a;
+    }
+    else {
+        return a + multiplicacion(a, b-1);
+    }
+};
+
+int main(){
+    int f;
+    int s;
+    printf("Ingrese el primer numero\n");
+    scanf("%d", &f);
+    printf("Ingrese el primer numero\n");
+    scanf("%d", &s);
+    int r = multiplicacion(f,s);
+    printf("el resultado es %d", r);
+    system ("pause");
+    printf("el resultado es %d", r);
+    return 0;
+}
